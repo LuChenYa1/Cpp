ec64a05f2a770040d5c4b59684425cf0190b93dc
lu-chen-ya <1934600272@qq.com>
2024-09-27 22:08:38 +0800

    24.9.27：类、对象：设计、权限

---
+++ C++_study/类和对象-封装-设计类.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+
+//! C++面向对象的三大特性：封装、继承、多态
+//! 对象都有其属性和行为
+//! 具有相同性质的对象，可以抽象为类
+
+//! 类中的属性和行为，统一称为成员
+//! 属性  成员属性 成员变量
+//! 行为  成员方法 成员函数
+
+const double PI = 3.14;
+
+class Circle
+{
+    //* 访问权限
+    //公共权限
+    public:
+    //* 属性
+    //* 半径
+    int Radius;
+    //* 行为
+    double GetPerimeter()
+    {
+        return 2 * PI * Radius;
+    }
+};
+
+class Student
+{
+    //公共权限
+    public:
+    //属性
+    string Name;
+    int ID;
+    //行为
+    //显示姓名和学号
+    void ShowStudent()
+    {
+        cout << "姓名: " << Name << "  学号:" << ID << endl;
+    }
+    //* 给姓名赋值
+    void SetName(string S_Name)
+    {
+        Name = S_Name;
+    }
+    //* 给学号赋值
+    void SetID(int S_ID)
+    {
+        ID = S_ID;
+    }
+};
+
+int main(void)
+{
+    //* 通过类，创建具体的对象
+    //* 实例化 
+    Circle C1;
+    //* 给对象的属性赋值
+    C1.Radius = 10;
+    //* 对象行为：获取它的周长
+    cout << "圆的半径是" << C1.GetPerimeter() << endl;
+
+    Student S1;
+    // S1.Name = "张三";
+    S1.SetName("张三");
+    // S1.ID = 1;
+    S1.SetID(1);
+    S1.ShowStudent();
+    Student S2;
+    S2.Name = "李四";
+    S2.ID = 2;
+    S2.ShowStudent();
+
+    return 0;
+}
---
+++ C++_study/类和对象-访问权限.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+//! 访问权限有三种：(默认私有权限)
+//! 1. public    公共权限  类内可以访问，类外可以访问
+//! 2. protected 保护权限  类内可以访问，类外不可以访问
+//! 3. private   私有权限  类内可以访问，类外不可以访问 儿子不可以访问父亲（继承）
+
+//! struct 和 class 很像，但struct 不能有成员方法，且默认公共权限
+
+//! 如何设置读写权限？
+//* 1、将属性设置为私有/保护
+//* 2、利用行为访问属性，根据需求进行读写
+
+class Test
+{
+    int a;
+};
+
+class Person
+{
+public:
+    string ReadName()
+    {
+        return Name;
+    }
+    void SetAge(int S_Age)
+    {
+        if(S_Age < 0 || S_Age > 150)
+        {
+            cout << "年龄设置不合理，请重新设置" << endl;
+            return;
+        }
+        Age = S_Age;
+    }
+    double ReadHeight()
+    {
+        return Height;
+    }
+    void SetHeight(double S_Height)
+    {
+        Height = S_Height;
+    }
+
+    //* 使用成员函数判断两对象的身高属性是否相等
+    bool isSameHeightByClass(Person C)//也可以引用，不过没必要
+    {
+        if(Height == C.ReadHeight())
+            return true;
+        return false;
+    }
+
+private:
+    string Name;//只读
+    int Age;//只写
+    double Height;//可读可写
+};
+
+//* 使用全局函数判断两对象的身高属性是否相等 
+bool isSameHeight(Person C1, Person C2)//也可以引用，不过没必要
+{
+    if(C1.ReadHeight() == C2.ReadHeight())
+        return true;
+    return false;
+}
+
+int main(void)
+{
+    //默认私有权限，成员不可访问
+    // Test T1;
+    // T1.a = 1;
+
+    Person P1;
+    cout << "姓名：" <<  P1.ReadName() << endl;
+    P1.SetAge(151);
+    P1.SetHeight(157);
+    cout << "身高：" <<  P1.ReadHeight() << endl;
+
+    Person P2;
+    P2.SetHeight(156);
+    // bool ret = isSameHeight(P1, P2);
+    bool ret = P1.isSameHeightByClass(P2);
+    if(ret == true)
+        cout << "身高一样" << endl;
+    else
+        cout << "身高不一样" << endl;
+    return 0;
+}
